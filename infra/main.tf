f12e98ac89cb4d0096de03b323846da31b4f1df3
osagiefe <felix.osagie@hotmail.com>
2025-07-25 19:58:40 -0400

    pull2

--- infra/main.tf
+++ infra/main.tf
@@ -26,3 +26,17 @@ resource "aws_instance" "remote-server1" {
     Name = "${var.env_prefix}-remote-server1"
   }
 }
+resource "aws_instance" "remote-server2" {
+  #ami                         = data.aws_ami.latest-amazon-linux-image.id
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  key_name                    = "jenkins"
+  subnet_id                   = aws_subnet.myapp-subnet-3.id
+  vpc_security_group_ids      = [aws_default_security_group.default-sg.id]
+  availability_zone           = var.avail_zone
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "${var.env_prefix}-remote-server2"
+  }
+}
