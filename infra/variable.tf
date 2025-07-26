5c26cef737afcdab7cc3de4ea8203c5d2421d257
osagiefe <felix.osagie@hotmail.com>
2025-07-25 20:07:52 -0400

    pull3

--- infra/variable.tf
+++ infra/variable.tf
@@ -6,3 +6,4 @@ variable "instance_type" {}
 variable "ami" {}
 variable "region" {}
 variable "key_name" {}
+variable "
--- infra/vpc.tf
+++ infra/vpc.tf
@@ -1,62 +1,80 @@
-#Create VPC and two public subnets
-resource "aws_vpc" "vpc" {
-  cidr_block = var.cidr_vpc
-  enable_dns_hostnames = true
-  enable_dns_support = true
-  instance_tenancy = "default"
-  
+resource "aws_vpc" "myapp-vpc" {
+  cidr_block = var.vpc_cidr_block
   tags = {
-    Name = "app-vpc"
+    Name = "${var.env_prefix}-vpc"
   }
 }
 
-resource "aws_subnet" "pub_sub_1" {
-  vpc_id     = aws_vpc.vpc.id
-  cidr_block = var.cidr_subnet_1
+resource "aws_subnet" "myapp-subnet-1" {
+  vpc_id            = aws_vpc.myapp-vpc.id
+  cidr_block        = var.subnet_cidr_block1
   availability_zone = var.avail_zone
-
   tags = {
-    Name = "subnet_1"
+    Name = "${var.env_prefix}-subnet-1"
   }
 }
-
-resource "aws_subnet" "pub_sub_2" {
-  vpc_id     = aws_vpc.vpc.id
-  cidr_block = var.cidr_subnet_2
+resource "aws_subnet" "myapp-subnet-2" {
+  vpc_id            = aws_vpc.myapp-vpc.id
+  cidr_block        = var.subnet_cidr_block2
   availability_zone = var.avail_zone
-
   tags = {
-    Name = "subnet_2"
+    Name = "${var.env_prefix}-subnet-2"
+  }
+}
+resource "aws_subnet" "myapp-subnet-3" {
+  vpc_id            = aws_vpc.myapp-vpc.id
+  cidr_block        = var.subnet_cidr_block3
+  availability_zone = var.avail_zone
+  tags = {
+    Name = "${var.env_prefix}-subnet-3"
   }
 }
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.vpc.id
-
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
   tags = {
-    Name = "app-vpc-igw"
+    Name = "${var.env_prefix}-igw"
   }
 }
 
-resource "aws_route_table" "pub_rt" {
-  vpc_id = aws_vpc.vpc.id
-
+resource "aws_default_route_table" "main-rtb" {
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
+    gateway_id = aws_internet_gateway.myapp-igw.id
   }
-
   tags = {
-    Name = "pub_rt"
+    Name = "${var.env_prefix}-main-rtb"
   }
 }
 
-resource "aws_route_table_association" "sub_1" {
-  subnet_id = aws_subnet.pub_sub_1.id
-  route_table_id = aws_route_table.pub_rt.id
-}
-
-resource "aws_route_table_association" "sub_2" {
-  subnet_id = aws_subnet.pub_sub_2.id
-  route_table_id = aws_route_table.pub_rt.id
+resource "aws_default_security_group" "default-sg" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "${var.env_prefix}-default-sg"
+  }
 }
